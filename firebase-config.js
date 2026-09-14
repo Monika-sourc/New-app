99ce516ece5f1985e487036f09f6c7b12d963417
Emmanuel Sognihoue <60749739facebook@gmail.com>
2026-09-14 17:38:12 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXaxTRKURvL7jLh9bmIz0TZyvciaPpQzs",
+  authDomain: "new-app-e6401.firebaseapp.com",
+  projectId: "new-app-e6401",
+  storageBucket: "new-app-e6401.firebasestorage.app",
+  messagingSenderId: "1005248947843",
+  appId: "1:1005248947843:web:f6708f80b03d9fd7835db6"
+};
